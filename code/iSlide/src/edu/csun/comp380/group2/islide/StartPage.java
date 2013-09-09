1ec2af3e9b672f97081c44270967d5a915718dcd
T Thomas <tyler.thomas.127@gmail.com>
2013-09-08 17:40:53 -0700

    Project setup
    Initial set up of Android project

---
+++ code/iSlide/src/edu/csun/comp380/group2/islide/StartPage.java
@@ -0,0 +1,23 @@
+package edu.csun.comp380.group2.islide;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start_page, menu);
+        return true;
+    }
+    
+}
