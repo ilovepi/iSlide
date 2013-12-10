e429dcd206b8fba6f366f000d862b8fb6d88705e
Paul Kirth <pk1574@gmail.com>
2013-12-09 20:48:30 -0800

    Revert "last commit to master, trying to configure github to switch branches"
    This reverts commit 134b0d42525f51b405cfb89dcd544a0590a77d5c.

--- code/iSlide/src/edu/csun/comp380/group2/islide/entity/PuzzleManager.java
+++ code/iSlide/src/edu/csun/comp380/group2/islide/entity/PuzzleManager.java
@@ -2,7 +2,6 @@ package edu.csun.comp380.group2.islide.entity;
 
 import java.util.ArrayList;
 
-import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
@@ -10,146 +9,62 @@ import org.andengine.ui.activity.BaseGameActivity;
 
 import edu.csun.comp380.group2.islide.util.GameConst;
 
-//Update Handler allows it to update on the game loop
-public class PuzzleManager implements IUpdateHandler {
+public class PuzzleManager {
 
-	private int columns, rows, width, height, tileWidth, tileHeight;
-	
+	private int columns, rows;
+	private int width, height;
+	private int tileWidth, tileHeight;
+	private int heightOffset;
 	private String assetFilename;
-	
 	private BitmapTextureAtlas puzzleTextureAtlas;
 	private BaseGameActivity activity;
-	private SlideTile[][] gridTiles;
-	
-	
-	// Important, Tells us if the user has completed the puzzle
-	private boolean puzzleComplete;
+	private ArrayList<SlideTile> tiles;
 
-	public PuzzleManager(int pWidth, int pHeight, int pColumns, int pRows, BitmapTextureAtlas puzzleBitmapTextureAtlas, BaseGameActivity pActivity, String pAssetFilename) {
-
-		init(pWidth, pHeight, pColumns, pRows, puzzleBitmapTextureAtlas, pActivity, pAssetFilename);
+	public PuzzleManager(int pWidth, int pHeight, int pColumns, int pRows,
+			BitmapTextureAtlas puzzleBitmapTextureAtlas,
+			BaseGameActivity pActivity, String pAssetFilename) {
+		init(pWidth, pHeight, pColumns, pRows, puzzleBitmapTextureAtlas,
+				pActivity, pAssetFilename);
 	}
 
-	private void init(int pWidth, int pHeight, int pColumns, int pRows, BitmapTextureAtlas puzzleBitmapTextureAtlas, BaseGameActivity pActivity, String pAssetFilename) {
-		this.currentTile = null;
-		this.locked = false;
-		this.puzzleComplete = false;
-		this.columns = pColumns > GameConst.getInstance().getMaxPuzzleColumns() ? GameConst.getInstance().getMaxPuzzleColumns() : pColumns;
-		this.rows = pRows > GameConst.getInstance().getMaxPuzzleRows() ? GameConst.getInstance().getMaxPuzzleRows() : pRows;
+	private void init(int pWidth, int pHeight, int pColumns, int pRows,
+			BitmapTextureAtlas puzzleBitmapTextureAtlas, BaseGameActivity pActivity, String pAssetFilename) {
 		
+		heightOffset = 0; //Don't Know if we necessarily want to deal with a height offset
+		
+		this.tiles = new ArrayList<SlideTile>();
+		this.columns = pColumns > GameConst.getInstance().getMaxPuzzleColumns() ? GameConst
+				.getInstance().getMaxPuzzleColumns() : pColumns;
+		this.rows = pRows > GameConst.getInstance().getMaxPuzzleRows() ? GameConst
+				.getInstance().getMaxPuzzleRows() : pRows;
+
 		this.assetFilename = pAssetFilename;
 		this.puzzleTextureAtlas = puzzleBitmapTextureAtlas;
-		
 		this.width = pWidth;
 		this.height = pHeight;
-		this.activity = pActivity;	
-
+		this.activity = pActivity;
+		
 		this.tileWidth = pWidth / columns;
 		this.tileHeight = pHeight / rows;
 		
-		ITiledTextureRegion mRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(puzzleBitmapTextureAtlas, activity, pAssetFilename, 0, 0, columns, rows);
+		ITiledTextureRegion mRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(puzzleBitmapTextureAtlas, activity, pAssetFilename,0,0, columns, rows);
 		puzzleBitmapTextureAtlas.load();
 		
-		gridTiles = new SlideTile[pColumns][pRows];
-		int tempId = 1;
-		
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				
-				if(j == rows - 1 && i == columns -1){
-					gridTiles[i][j] = new EmptyTile(j*tileWidth, i*tileHeight, (tempId-1) , mRegion, activity.getVertexBufferObjectManager(),0, this);
-					break;
-				}
-				
-				gridTiles[i][j] = new SlideTile(j*tileWidth, i*tileHeight, (tempId-1) , mRegion, activity.getVertexBufferObjectManager(),tempId++, this);
-				
-			}
+		/*
+		 * Order of puzzle initialization 
+		 * [0] [1] [2] 
+		 * [3] [4] [5] 
+		 * [6] [7] X
+		 */
+		for(int i = 0; i < (columns * rows) - 1; i++ )
+		{
+			tiles.add(new SlideTile((i%columns) * tileWidth , ((i/rows) * tileHeight) + heightOffset, i, mRegion, activity.getVertexBufferObjectManager()));
 		}
+
 	}
-	
-	
-	SlideTile currentTile;
-	public boolean locked;
-	
-	@Override
-	public void onUpdate(float pSecondsElapsed) {
-		if(this.currentTile == null){
-			for(int i = 0; i < gridTiles[0].length; i++){
-				for(int j = 0; j < gridTiles.length; j++){
-					if(gridTiles[i][j].isPressed){
-						currentTile = gridTiles[i][j];
-						locked = true;
-						
-					}
-				}
-			}
-		}
-		else{
-			if(!currentTile.isPressed){
-				currentTile = null;
-				locked = false;
-			}
-		}
-	}
-	
-	public void requestMove(SlideTile tile, SlideTile.Direction direction)
-	{
-		int iIndex = 0, jIndex = 0;
-		
-		for(int i = 0 ; i < gridTiles[0].length; i++){
-			for(int j = 0; j < gridTiles.length; j++){
-				if(tile.id == gridTiles[j][i].id){
-					iIndex = i; jIndex = j;
-					break;
-				}
-			}
-		}
-		
-		SlideTile tempTile;
-		if(direction == SlideTile.Direction.RIGHT && gridTiles[jIndex][iIndex + 1] != null && gridTiles[jIndex ][iIndex + 1].id == 0 ){
-			
-			tempTile = gridTiles[jIndex][iIndex + 1];
-			gridTiles[jIndex][iIndex + 1] = tile;
-			gridTiles[jIndex][iIndex] = tempTile;
-		}
-		else if(direction == SlideTile.Direction.LEFT && gridTiles[jIndex-1][iIndex] != null && gridTiles[jIndex-1][iIndex].id == 0 ){
-			tempTile = gridTiles[jIndex-1][iIndex];
-			gridTiles[jIndex-1][iIndex] = tile;
-			gridTiles[jIndex][iIndex] = tempTile;
-		}
-		else if(direction == SlideTile.Direction.UP && gridTiles[jIndex][iIndex + 1] != null && gridTiles[jIndex][iIndex + 1].id == 0 ){
-			tempTile = gridTiles[jIndex][iIndex+1];
-			gridTiles[jIndex][iIndex+1] = tile;
-			gridTiles[jIndex][iIndex] = tempTile;
-		}
-		else if(direction == SlideTile.Direction.DOWN && gridTiles[jIndex][iIndex - 1] != null && gridTiles[jIndex][iIndex-1].id == 0 ){
-			tempTile = gridTiles[jIndex][iIndex-1];
-			gridTiles[jIndex][iIndex-1] = tile;
-			gridTiles[jIndex][iIndex] = tempTile;
-		}
-		else {
-			
-			return;
-		}
-		float tempX = tempTile.getX(), tempY = tempTile.getY();
-		tempTile.setX(tile.getX());
-		tempTile.setY(tile.getY());
-		
-		tile.setX(tempX);
-		tile.setY(tempY);
-	}
-	@Override
-	public void reset() {
+
+	public ArrayList<SlideTile> getTiles() {
 		// TODO Auto-generated method stub
-
+		return this.tiles;
 	}
-	
-	public boolean getPuzzleCompleted() {
-		return this.puzzleComplete;
-	}
-	public SlideTile[][] getTiles() {
-		return this.gridTiles;
-	}
-	
-
 }
