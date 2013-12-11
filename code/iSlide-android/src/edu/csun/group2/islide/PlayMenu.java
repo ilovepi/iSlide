506e4112b750eb528387cea75bee277e7ff71b7f
T Thomas <tyler.thomas.127@gmail.com>
2013-12-10 21:54:21 -0800

    Ok i swear last push for the night, just added an API check to see if the camera can be used. we wouldnt want to exclude customers just because their phones are older

--- code/iSlide-android/src/edu/csun/group2/islide/PlayMenu.java
+++ code/iSlide-android/src/edu/csun/group2/islide/PlayMenu.java
@@ -1,11 +1,16 @@
 package edu.csun.group2.islide;
 
+import javax.xml.datatype.Duration;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class PlayMenu extends Activity {
@@ -15,6 +20,7 @@ public class PlayMenu extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play_menu);
 		setButtonHandlers();
+		
 	}
 	private void setButtonHandlers() {
 	    ((Button) findViewById(R.id.btn_select)).setOnClickListener(btnClick);  
@@ -34,8 +40,13 @@ public class PlayMenu extends Activity {
 	            }
 	            //add camera interaction here
 	            case R.id.btn_select: {
-	            	Intent select = new Intent(PlayMenu.this, TakePictureActivity.class);
-	            	startActivity(select);
+	            	if(android.os.Build.VERSION.SDK_INT >= 9){
+	            		Intent select = new Intent(PlayMenu.this, TakePictureActivity.class);
+	            		startActivity(select);
+	            	}
+	            	else{
+	            		Toast.makeText(PlayMenu.this, "Sorry API Level not high enough to use Camera function.", Toast.LENGTH_LONG).show();
+	            	}
 	                break;
 	            }
 	            /*
